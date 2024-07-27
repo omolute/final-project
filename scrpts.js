69ab7522e9626236160d0b1e3ba9739c9a8b3def
omolute <omolojaishaq@gmail.com>
2024-07-27 12:32:53 +0100

    init commit

---
+++ scrpts.js
@@ -0,0 +1,8 @@
+function open_nav(){
+    document.getElementById("nav_sidebar").style.display = "block";
+    document.getElementById("nav_hamburger").style.display = "none";
+}
+function close_nav(){
+    document.getElementById("nav_sidebar").style.display = "none";
+    document.getElementById("nav_hamburger").style.display = "block";
+}
